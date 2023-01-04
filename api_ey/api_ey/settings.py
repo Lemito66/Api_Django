887e91841f636e0f2be942fae9c8b4cb68ea32e2
Emill <lemito66@gmail.com>
2023-01-04 10:45:05 -0500

    Eliminación de keys

--- api_ey/api_ey/settings.py
+++ api_ey/api_ey/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1_bs=3*2-(lfj0jt$wy%*c%a$wczh=kem#6%5%%!v6x#r%la0p'
+#pip install python-decouple
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
